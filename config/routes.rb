6891e334337d831e934a27e33c6754c5e40feb05
toanmx0101 <wth8647@gmail.com>
2017-09-09 06:10:49 +0700

    Added search form and controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :articles
+  resources :articles do
+    collection { get :search }
+  end
+
   root to: 'articles#index'
   resources :articles do
     collection { get :search }
