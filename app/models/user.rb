856674548d1a926cfbd5744203a54e128a010709
toanmx0101 <wth8647@gmail.com>
2018-01-26 13:54:09 +0700

    welcome after signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :registerable, :confirmable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
